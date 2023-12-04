4c456ca8dcaa7fa46cb854a303affb0fe4ae683a
deadlycatco <helly.miners121@gmail.com>
2023-12-05 00:23:37 +0200

    gif moves, check if cell free, moves to free cell

---
+++ src/main/java/com/example/myjavafxapp/BaseModel.java
@@ -0,0 +1,56 @@
+package com.example.myjavafxapp;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class BaseModel {
+    private String Gif_url = "https://img.itch.zone/aW1nLzMzMzY4OTguZ2lm/original/0Ut41Y.gif";
+    private Image gifImage;
+    private ImageView gifImageView;
+
+    private int positionX;
+    private int positionY;
+
+    public BaseModel(){
+        this.positionY = 0;
+        this.positionX = 0;
+        this.gifImage = new Image(Gif_url);
+        this.gifImageView = new ImageView(gifImage);
+    }
+    public BaseModel(String gif_url){
+        this.Gif_url = gif_url;
+    }
+
+
+    public String getGif_url() {
+        return Gif_url;
+    }
+    public void setGif_url(String gif_url) {
+        Gif_url = gif_url;
+    }
+    public Image getGifImage() {
+        return gifImage;
+    }
+    public void setGifImage(Image gifImage) {
+        this.gifImage = gifImage;
+    }
+    public ImageView getGifImageView() {
+        return gifImageView;
+    }
+    public void setGifImageView(ImageView gifImageView) {
+        this.gifImageView = gifImageView;
+    }
+    public int getPositionX() {
+        return positionX;
+    }
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+    public int getPositionY() {
+        return positionY;
+    }
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+
+}
--- src/main/java/com/example/myjavafxapp/HelloApplication.java
+++ src/main/java/com/example/myjavafxapp/HelloApplication.java
@@ -50,33 +50,44 @@ public class HelloApplication extends Application {
                 gridPane.add(cell, col, row);
             }
         }
-        Pane cell = getCell(gridPane, 0,0);
-        Image gifImage = new Image(GIF_URL);
-        ImageView gifImageView = new ImageView(gifImage);
 
+        Pane cell1 = getCell(gridPane, 2,2);
+        BaseModel baseModel1 = new BaseModel();
+        // Set the fit width and fit height to be a fraction of the cell size
+        double cellWidth1 = cell1.getMinWidth();
+        double cellHeight1 = cell1.getMinHeight();
+        baseModel1.getGifImageView().setFitWidth(cellWidth1 * 0.95); // Adjust the factor based on your preference
+        baseModel1.getGifImageView().setFitHeight(cellHeight1 * 0.95);
+        baseModel1.setPositionX(2);
+        baseModel1.setPositionY(2);
+        cell1.getChildren().add(baseModel1.getGifImageView());
+
+        Pane cell = getCell(gridPane, 5,5);
+
+        BaseModel baseModel = new BaseModel();
         // Set the fit width and fit height to be a fraction of the cell size
         double cellWidth = cell.getMinWidth();
         double cellHeight = cell.getMinHeight();
-        gifImageView.setFitWidth(cellWidth * 1.2); // Adjust the factor based on your preference
-        gifImageView.setFitHeight(cellHeight * 1.2);
+        baseModel.getGifImageView().setFitWidth(cellWidth * 0.95); // Adjust the factor based on your preference
+        baseModel.getGifImageView().setFitHeight(cellHeight * 0.95);
+        baseModel.setPositionX(5);
+        baseModel.setPositionY(5);
 
-        cell.getChildren().add(gifImageView);
-        moveGifToRight(cell, 0, 0);
+        cell.getChildren().add(baseModel.getGifImageView());
+        System.out.println(isCellFree(gridPane, 2,2));
+        //if (isCellFree(gridPane, 2,2)){
+           // moveBaseModel(gridPane,baseModel, 2,2);
+       // } else {
+           // moveBaseModel(gridPane,baseModel, 3,3);
+       // }
+        //moveGifToRight(cell, 0, 0);
+        moveBaseModel(gridPane, baseModel);
+        moveBaseModel(gridPane, baseModel);
+        moveBaseModel(gridPane, baseModel);
+        moveBaseModel(gridPane, baseModel);
 
 
-//        for(int i = 0; i < 7; i++){
-//            Pane targetCell = getCell(gridPane, i, i);
-//            moveGif(cell, targetCell);
-//        }
 
-
-//        Button moveButton = new Button("Move GIF");
-//        moveButton.setOnAction(new EventHandler<ActionEvent>() {
-//            @Override
-//            public void handle(ActionEvent event) {
-//                moveGifFromCellToCell(cell, 1, 1);
-//            }
-//        });
 
         //VBox vBox = new VBox(stackPane, moveButton);
         BorderPane root = new BorderPane();
@@ -105,63 +116,167 @@ public class HelloApplication extends Application {
         return null;
     }
 
-    private void moveGifToRight(Pane sourceCell, int targetRow, int targetCol) {
-        if (sourceCell.getChildren().size() > 0) {
-            ImageView gifImageView = (ImageView) sourceCell.getChildren().get(0);
+    public boolean isCellFree(GridPane gridPane, int targetRow, int targetCol){
+        Pane cell = getCell(gridPane, targetRow, targetCol);
+        if(!cell.getChildren().isEmpty()){
+                return false;
+        }
+        return true;
+    }
+    public void moveBaseModel(GridPane gridPane, BaseModel baseModel) {
+        int currentX = baseModel.getPositionX();
+        int currentY = baseModel.getPositionY();
 
-            // Create a new TranslateTransition
-            TranslateTransition transition = new TranslateTransition(new Duration(10000), gifImageView);
+        // Search for an available cell around the current position
+        for (int offsetX = -1; offsetX <= 1; offsetX++) {
+            for (int offsetY = -1; offsetY <= 1; offsetY++) {
+                int targetRow = currentX + offsetX;
+                int targetCol = currentY + offsetY;
 
-
-            // Set the destination coordinates
-            double targetX = getCell(gridPane, targetRow, targetCol).getLayoutX() + 1 * cellWidth;
-            double targetY = getCell(gridPane, targetRow, targetCol).getLayoutY() + 0 * cellHeight;
-
-            // Set the translation
-            transition.setToX(targetX);
-            transition.setToY(targetY);
-
-            // Play the transition
-            transition.play();
-
-            // Remove the gif from the source cell
-            sourceCell.getChildren().remove(gifImageView);
-
-            // Add the gif to the target cell
-            Pane targetCell = getCell(gridPane, targetRow, targetCol);
-            targetCell.getChildren().add(gifImageView);
+                if (isCellFree(gridPane, targetRow, targetCol)) {
+                    moveBaseModelTo(gridPane, baseModel, targetRow, targetCol);
+                    return; // Move only once to the first available cell
+                }
+            }
         }
     }
 
-    private void moveGif(Pane sourceCell, Pane targetCell) {
-        if (sourceCell.getChildren().size() > 0) {
-            ImageView gifImageView = (ImageView) sourceCell.getChildren().get(0);
+    private void moveBaseModelTo(GridPane gridPane, BaseModel baseModel, int targetRow, int targetCol) {
+        int currentX = baseModel.getPositionX();
+        int currentY = baseModel.getPositionY();
 
-            // Set the initial position of the gif to the top-left corner of the source cell
-            gifImageView.setLayoutX(0);
-            gifImageView.setLayoutY(0);
+        double cellWidth = this.cellWidth;
+        double cellHeight = this.cellHeight;
 
-            // Create a new TranslateTransition
-            TranslateTransition transition = new TranslateTransition(new Duration(1000), gifImageView);
+        double newTranslateX = (targetRow - currentX) * cellWidth;
+        double newTranslateY = (targetCol - currentY) * cellHeight;
 
-            // Set the destination position relative to the target cell
-            transition.setToX(targetCell.getLayoutX() - sourceCell.getLayoutX());
-            transition.setToY(targetCell.getLayoutY() - sourceCell.getLayoutY());
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(3), baseModel.getGifImageView());
+        transition.setToX(newTranslateX);
+        transition.setToY(newTranslateY);
+        transition.play();
 
-            // Play the transition
-            transition.play();
+        baseModel.setPositionX(targetRow);
+        baseModel.setPositionY(targetCol);
 
-            // Remove the gif from the source cell after the transition completes
-            transition.setOnFinished(event -> {
-                sourceCell.getChildren().remove(gifImageView);
+        transition.setOnFinished(event -> {
+            Pane oldCell = getCell(gridPane, currentX, currentY);
+            oldCell.getChildren().remove(baseModel.getGifImageView());
 
-                // Add the gif to the target cell
-                targetCell.getChildren().add(gifImageView);
-            });
-        }
+            Pane newCell = getCell(gridPane, targetRow, targetCol);
+            newCell.getChildren().add(baseModel.getGifImageView());
+
+            baseModel.getGifImageView().setTranslateX(0);
+            baseModel.getGifImageView().setTranslateY(0);
+        });
     }
+//    public void moveBaseModel(GridPane gridPane, BaseModel baseModel, int targetRow, int targetCol) {
+//        // Отримуємо поточні координати клітинки
+//        int currentX = baseModel.getPositionX();
+//        int currentY = baseModel.getPositionY();
+//
+//        // Отримуємо розмір клітинки
+//        double cellWidth = this.cellWidth;
+//        double cellHeight = this.cellHeight;
+//
+//        // Оновлюємо розташування gifImageView в новій клітинці
+//        double newTranslateX;
+//        double newTranslateY;
+//        if (baseModel.getPositionX() > targetRow){
+//             newTranslateX = (baseModel.getPositionX() -targetRow) * -cellWidth;
+//        }else {
+//             newTranslateX = (targetRow - baseModel.getPositionX()) * cellWidth;
+//        }
+//        if (baseModel.getPositionY() > targetCol){
+//             newTranslateY = (baseModel.getPositionY()- targetCol) * -cellHeight;
+//        }else {
+//             newTranslateY = (targetCol - baseModel.getPositionY()) * cellHeight;
+//        }
+////        newTranslateX = -targetRow * cellWidth;
+////        newTranslateY = -targetCol * cellHeight;
+//        // Створюємо TranslateTransition для анімації переміщення
+//         TranslateTransition transition = new TranslateTransition(Duration.seconds(5), baseModel.getGifImageView());
+//        transition.setToX(newTranslateX);
+//        transition.setToY(newTranslateY);
+//        transition.play();
+//
+//        // Змінюємо координати базової моделі
+//        baseModel.setPositionX(targetRow);
+//        baseModel.setPositionY(targetCol);
+//
+//        // Видаляємо gifImageView зі старої клітинки після завершення анімації
+//        transition.setOnFinished(event -> {
+//            Pane oldCell = getCell(gridPane, currentX, currentY);
+//            oldCell.getChildren().remove(baseModel.getGifImageView());
+//
+//            // Додаємо gifImageView до нової клітинки
+//            Pane newCell = getCell(gridPane, targetRow, targetCol);
+//            newCell.getChildren().add(baseModel.getGifImageView());
+//
+//            // Очищаємо трансформації, щоб уникнути зайвих зсувів
+//            baseModel.getGifImageView().setTranslateX(0);
+//            baseModel.getGifImageView().setTranslateY(0);
+//        });
+//    }
 
-
+//
+//    private void moveGifToRight(Pane sourceCell, int targetRow, int targetCol) {
+//        if (sourceCell.getChildren().size() > 0) {
+//            ImageView gifImageView = (ImageView) sourceCell.getChildren().get(0);
+//
+//            // Create a new TranslateTransition
+//            TranslateTransition transition = new TranslateTransition(new Duration(10000), gifImageView);
+//
+//
+//            // Set the destination coordinates
+//            double targetX = getCell(gridPane, targetRow, targetCol).getLayoutX() + 1 * cellWidth;
+//            double targetY = getCell(gridPane, targetRow, targetCol).getLayoutY() + 2 * cellHeight;
+//
+//            // Set the translation
+//            transition.setToX(targetX);
+//            transition.setToY(targetY);
+//
+//            // Play the transition
+//            transition.play();
+//
+//            // Remove the gif from the source cell
+//            sourceCell.getChildren().remove(gifImageView);
+//
+//            // Add the gif to the target cell
+//            Pane targetCell = getCell(gridPane, targetRow, targetCol);
+//            targetCell.getChildren().add(gifImageView);
+//        }
+//    }
+//
+//    private void moveGif(Pane sourceCell, Pane targetCell) {
+//        if (sourceCell.getChildren().size() > 0) {
+//            ImageView gifImageView = (ImageView) sourceCell.getChildren().get(0);
+//
+//            // Set the initial position of the gif to the top-left corner of the source cell
+//            gifImageView.setLayoutX(0);
+//            gifImageView.setLayoutY(0);
+//
+//            // Create a new TranslateTransition
+//            TranslateTransition transition = new TranslateTransition(new Duration(1000), gifImageView);
+//
+//            // Set the destination position relative to the target cell
+//            transition.setToX(targetCell.getLayoutX() - sourceCell.getLayoutX());
+//            transition.setToY(targetCell.getLayoutY() - sourceCell.getLayoutY());
+//
+//            // Play the transition
+//            transition.play();
+//
+//            // Remove the gif from the source cell after the transition completes
+//            transition.setOnFinished(event -> {
+//                sourceCell.getChildren().remove(gifImageView);
+//
+//                // Add the gif to the target cell
+//                targetCell.getChildren().add(gifImageView);
+//            });
+//        }
+//    }
+//
+//
 
     public static void main(String[] args) {
         launch(args);
